e4b8bd4c57ca34671ada06e80917234522437c94
nickconlon <nickconlon1@gmail.com>
2017-10-09 14:39:44 -0400

    working localization

--- hw4/hw_4_localization/src/pybot.py
+++ hw4/hw_4_localization/src/pybot.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+# Nicholas Conlon
+# Basic probram for localization using a bayes filter given a 
+# 2D environment, a map, and some sensor readings.
+
 import numpy as np
 import rospy
+import sys
 import sensing_model_and_map as prob
 import plot as plt
 
@@ -11,9 +16,9 @@ from geometry_msgs.msg import Twist
 doorReading = True
 
 #
-# The callback for receibing sensor readings
+# The callback for receiving sensor readings.
 #
-def callback(data): #todo delete me for speed!
+def callback(data):
   rospy.loginfo("I heard %s", data.data)
   global doorReading
   if data.data == 'Door':
@@ -22,85 +27,108 @@ def callback(data): #todo delete me for speed!
     doorReading = False
 
 #
-# The bayes filter
+# The bayes filter.
 #
 def bayesFilter(bel, u, d):
-  n = 0
-  bel_ = [0]*600
-  
   if d == 'z':
-    for x in range(len(bel)):
+    bel_ = sensorUpdate(bel)
+  if d == 'u':
+    bel_ = movementUpdate(bel, u)
+  return norm(bel_)
+
+#
+# Apply a sensor update.
+#
+def sensorUpdate(bel):
+  bel_ = [0]*len(bel)
+  for x in range(len(bel)):
       if doorReading == True: #door reading
         bel_[x]=prob.p_door(x/10)*bel[x]
-        n = n + bel_[x]
       else: # wall reading
         bel_[x]=(1-prob.p_door(x/10))*bel[x]
-        n = n + bel_[x]
-    for x in range(len(bel)):
-      bel_[x]=bel_[x]/n;
-
-  if d == 'u':
-    belShift = move(bel, 4)#shift(bel)
-    for x in range(len(bel)):
-      bel_[x]=belShift[x]*bel[x]
-
   return bel_
 
 #
-# Shift (motion model)
+# Apply a movement update.
 #
-def shift(array):
-  for x in range(len(array)-1):
-    array[len(array)-x-1] = array[len(array)-x-2]
-  array[0]=1/600.0
-  return array
-
-def move(array, m):
-  n = len(array)
-  for x in range(n):
-    array[x] = array[(x-m)%n]
-  return array
+def movementUpdate(bel, m):
+  rolled = np.roll(bel, m) 
+  for x in range(abs(m)):
+    rolled[x] = 1/600 #just use some small non-zero probability.
+  return norm(rolled)
 
 #
-# The main loop
+# Normalize the array.
 #
-def bot():
+def norm(bel):
+  n = sum(bel)
+  if n != 0:
+    for x in range(len(bel)):
+      bel[x]=bel[x]/n
+  return bel
 
-  #initialize the pub's and sub's and ros things
+#
+# The main loop for this bot.
+#
+def bot(argv):
+
+  fail = False
+
+  if len(argv) == 2:
+    if argv[1] == 'east':
+      position = 600
+      velocity = -4
+    if argv[1] == 'west':
+      position = 0
+      velocity = 4
+    else:
+      fail = True
+  else:
+    fail = True
+
+
+  if fail == True:
+    print 'usage pybot.py <driving direction: \'east\' or \'west\'>'
+    sys.exit()
+
+  #initialize the pub's and sub's and ros things.
   pub = rospy.Publisher('robot/cmd_vel', Twist, queue_size=100)
   sub = rospy.Subscriber('robot/wall_door_sensor', String, callback)
   rospy.init_node('pybot', anonymous=True)
   rate = rospy.Rate(10)
   
-  #initialize the probelm specific things
+  #initialize the probelm specific things.
   hist = [1/600.0]*600
-  velocity = 4
-  position = 0
-
+  
   while not rospy.is_shutdown():
-    
-    hist = bayesFilter(hist, position, 'z')
+   
+    # Apply the bayes filter for a sensor reading. 
+    hist = bayesFilter(hist, velocity/2, 'z')
 
-    hist = shift(hist)
-    if position %20 == 0:
-      hist = bayesFilter(hist, position, 'u')
+    # Apply the beyes filter for a movement. 
+    hist = bayesFilter(hist, velocity/2, 'u') 
     
-    position = position + 4
-    print position
+    # Update the position.
+    position = position + velocity
     
+    # Plot the histogram.
     plt.test(hist)
+
+    # Send out some movement commands.
     twist = Twist()
     twist.linear.x = velocity
     pub.publish(twist)
+
+    # Do some zzz.
     rate.sleep()
 
 
 
 #
-# The thing that you do in pythong
+# The thing that you do in python.
 #
 if __name__=='__main__':
   try:
-    bot()
+    bot(sys.argv)
   except rospy.ROSInterruptException:
     pass
