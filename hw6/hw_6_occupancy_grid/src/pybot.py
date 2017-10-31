87831370fefac31dbc45e867002d61574a0a9d8a
nickconlon <nickconlon1@gmail.com>
2017-10-30 22:25:49 -0400

    semi-working occupancy grid

--- hw6/hw_6_occupancy_grid/src/pybot.py
+++ hw6/hw_6_occupancy_grid/src/pybot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Nicholas Conlon
-# Basic program for localization using a particle filter given a 
+# Basic program for occupancy grid mapping given a 
 # 2D environment, a map, and some sensor readings.
 
 import numpy as np
@@ -14,131 +14,140 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
+from nav_msgs.msg import OccupancyGrid
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import time
 
-
+# Some constatnts
 SENSOR_CONE_DEGREES = 45
-SCALE_FACTOR =1
+SCALE_FACTOR = 4
 GRID_MAX_X = SCALE_FACTOR*60
 GRID_MAX_Y = SCALE_FACTOR*16
-OFFSET_X = GRID_MAX_X/2
-OFFSET_Y = GRID_MAX_Y/2
+OFFSET_X = 30
+OFFSET_Y = 8
+
+# Twist message
+twist = Twist()
+
+# Occupancy grid message
+grid_msgs = OccupancyGrid()
+grid_msgs.header.frame_id = '/map'
+grid_msgs.info.resolution = 1./SCALE_FACTOR
+grid_msgs.info.width = GRID_MAX_X
+grid_msgs.info.height = GRID_MAX_Y
+grid_msgs.data = range(GRID_MAX_X*GRID_MAX_Y)
 
 class Data():
-  grey = -1#[155, 155, 155]
-  black =0.5# [255, 255, 255]
-  white =1# [1, 1, 1]
-  
-truth = Data()
-truth.img = [[truth.grey for x in range(GRID_MAX_X)] for j in range(GRID_MAX_Y)]
-truth.prob = [[0.5 for x in range(GRID_MAX_X)] for j in range(GRID_MAX_Y)]
-truth.x = None
-truth.y = None
-truth.theta = 0
+  def __init__(self, x, y, theta):
+    self.x = x
+    self.y = y
+    self.theta = theta
+    self.grid = np.array([[-1.0 for x in range(GRID_MAX_X)] for j in range(GRID_MAX_Y)], np.int8)
 
+# Robot state
+state = Data(None, None, 0.0)
 
+# Publishers
 pub = rospy.Publisher('robot/cmd_vel', Twist, queue_size=1)
+occ = rospy.Publisher('/map',OccupancyGrid, queue_size=1)
 
+
+#
+# Callback for State truth data
+#
+def truthCallback(data):
+  state.x = data.pose.pose.position.x
+  state.y = data.pose.pose.position.y
+  state.theta = get_rotation(data)
+  return
 
 
 #
 # The callback for receiving sensor readings.
 #
 def sensorCallback(data):
-  #rospy.loginfo("[045] = %s", data.ranges[1])
+  r1 = data.ranges[1] # to the right 45
+  r2 = data.ranges[2] # straight ahead
+  r3 = data.ranges[3] # to the left 45
   
-  
-  if(truth.x != None and truth.y != None):
-    y = int(truth.y*SCALE_FACTOR+OFFSET_Y)
-    x = int(truth.x*SCALE_FACTOR+OFFSET_X)
-    #truth.img[y][x]=truth.white
-    
-    for l in range(int(data.ranges[2])): # forward
-      x = int(truth.x*SCALE_FACTOR+OFFSET_X)
-      y = int(truth.y*SCALE_FACTOR+OFFSET_Y)
-      x = int(x+l*np.cos(truth.theta))
-      y = int(y+l*np.sin(truth.theta))
-      #truth.img[y][x]=truth.white
-    
+  if(state.x != None and state.y != None): #so we don't try to map before we get state information
+    x = point2d((state.x+OFFSET_X), (state.y+OFFSET_Y)) # current position
+    occupancyGridMapping(x, r1, r2, r3)
 
-    curPoint = point2d((truth.x+OFFSET_X)/SCALE_FACTOR, (truth.y+OFFSET_Y)/SCALE_FACTOR)
-    for i in range(GRID_MAX_X):# x
-      for j in range(GRID_MAX_Y): # y
-        mapPoint = point2d(i/SCALE_FACTOR+SCALE_FACTOR/2, j/SCALE_FACTOR+SCALE_FACTOR/2)
-        if(inPerceptualField(mapPoint, curPoint, truth.theta, data.ranges[2])):
-          truth.img[j][i]=truth.white
-
-    
-  pub.publish(getTwistToPublish(data.ranges[1], data.ranges[2], data.ranges[3]))
-  plt.plot(truth.img) 
+  pub.publish(getTwistToPublish(r1, r2, r3))
+  occ.publish(getGridToPublish(state.grid))
   return
 
+#
+# Fill in the grid message to publish
+#
+def getGridToPublish(msg):
+  grid_msgs.data = np.ravel(msg, order='C')
+  return grid_msgs
+
+
+#
+# Given some orientation message, return the yaw (heading estimate)
+#
 def get_rotation (msg):
     orientation_q = msg.pose.pose.orientation
     orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
     (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
     return yaw
 
+#
+# Inverse sensor model
+#
+def inverseSensorModel(mp, cp, z):
+  numerator = 1  #np.random.normal(
+  denominator = 1  #1-numerator
+  print np.log(numerator/denominator)
+  return 1
 
-def truthCallback(data):
-  truth.x = data.pose.pose.position.x
-  truth.y = data.pose.pose.position.y
-  truth.theta = get_rotation(data)
-  return
 
-
-def inverseSensorModel(m, x, z):
-  numerator = 1  #p(m|z, x)
-  denominator = 1  #1-p(m|z, x)
-  return np.log(numerator/denominator)
-
-def inPerceptualField(mapPoint, curPoint, heading, reading):
+#
+# Return true if mapPoint is in the perceptual field of the robot given it's state
+#
+def inPerceptualField(mapPoint, curPoint, heading_deg, reading):
   d = mapPoint.getDistance(curPoint) 
-  heading_deg = (np.rad2deg(heading)+360)%360
   theta_l = heading_deg+SENSOR_CONE_DEGREES
   theta_r = heading_deg-SENSOR_CONE_DEGREES
   theta_point = np.arctan2(mapPoint.y-curPoint.y, mapPoint.x-curPoint.x)
   theta_point = np.rad2deg(theta_point)
   if(d <= reading and theta_point < theta_l and theta_point > theta_r):
     return True
-
   return False
    
-
-# Point x
-# Point y
-def getCentroid(x, y):
-  return 
-
 
 # matrix l
 # Point m
 # Point x
 # double heading angle z
-def occupancyGridMapping(l, x, z):
-  for i in range(len(l)):
-    if(inPerceptualField(m, x, z)):
-      l[i] = l[i]+inverseSensorModel(m,x,z)-l0
-    else:
-      l[i] = l[i]
-  return l
+def occupancyGridMapping(cp, r1, r2, r3):
+
+  heading_deg = (np.rad2deg(state.theta)+360)%360
+  for y in range(GRID_MAX_Y):
+    for x in range(GRID_MAX_X):
+      mp = point2d(x/SCALE_FACTOR+SCALE_FACTOR/2, y/SCALE_FACTOR+SCALE_FACTOR/2)
+      if(inPerceptualField(mp, cp, heading_deg, r2)):
+        state.grid[y][x]=state.grid[y][x]+inverseSensorModel(mp, cp, r2) # -lo ? const prior
+  return
 
 
 
-
+#
+# Fill in the twist message based on current position data
+#
 def getTwistToPublish(r1, r2, r3):
-  twist = Twist()
-  twist.linear.x = 5.0
-
-  if(r1 < 2):
+  if(r1 < 2): # don't crash
     twist.linear.x = 0.0
     twist.angular.z = 10.0
-  elif(r1 < r3):
+  elif(r1 < r3): # don't go too far in either direction
     twist.angular.z = 2.0
+    twist.linear.x = 5.0
   else:
     twist.angular.z = -2.0
-
+    twist.linear.x = 5.0
   return twist
 
 
@@ -146,7 +155,6 @@ def getTwistToPublish(r1, r2, r3):
 # The main loop for this bot.
 #
 def bot(argv):
-
   rospy.init_node('pybot', anonymous=True)
   sensor_sub = rospy.Subscriber('robot/base_scan', LaserScan, sensorCallback, queue_size=1)
   truth_sub = rospy.Subscriber('stage/base_pose_ground_truth', Odometry, truthCallback, queue_size=1) 
